6d2266699f1e8e2ad560347e07bf1749af6340fb
Daniel Coley <dmcoley948562@gmail.com>
2017-06-30 11:32:30 -0600

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def dashboard
+  end
+
+  def trophy
+  end
+
+  def points
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'pages/dashboard'
+
+  get 'pages/trophy'
+
+  get 'pages/points'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
